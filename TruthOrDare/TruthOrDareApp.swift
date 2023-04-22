33ec893bb1338de3d131253100d8751dff6cd8da
JayJayaprakash <jayavardhanjp@gmail.com>
2023-04-22 23:26:12 +0100

    Initial Commit

---
+++ TruthOrDare/TruthOrDareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruthOrDareApp.swift
+//  TruthOrDare
+//
+//  Created by Jay Jayaprakash on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TruthOrDareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
